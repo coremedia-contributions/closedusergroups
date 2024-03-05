98612d66172fbd26095d18fca545940c036fbbad
Philip Hemptenmacher <philip.hemptenmacher@coremedia.com>
2024-03-05 17:24:44 +0100

    first step to add cug to headless server

--- closedusergroup-headless-server/src/main/java/com/coremedia/blueprint/cug/filter/HeadlessServerConfig.java
+++ closedusergroup-headless-server/src/main/java/com/coremedia/blueprint/cug/filter/HeadlessServerConfig.java
@@ -1,15 +1,12 @@
 package com.coremedia.blueprint.cug.filter;
 
 import com.coremedia.caas.headless_server.plugin_support.extensionpoints.FilterPredicate;
-import com.coremedia.cap.undoc.common.spring.CapRepositoriesConfiguration;
 import com.coremedia.personalization.context.ContextCollection;
 import com.coremedia.personalization.context.collector.ContextCollectionConfiguration;
-import com.coremedia.personalization.context.collector.ContextCollectorConfiguration;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @Import({
